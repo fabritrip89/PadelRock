4ca760c309110fb3acf2992916afc42ffb096d5d
Fabricio <fabriciot89@gmail.com>
2022-06-27 22:22:35 -0300

    asdasd

--- main/main.js
+++ main/main.js
@@ -1,25 +1,21 @@
-
-
 const search = () => {
-  const searchbox = document.getElementById("search-item").value.toUpperCase();
-const storieitems = document.getElementById ("contenedorPaletas")
-const product = document.querySelectorAll(".product")
-const pname =document.getElementsById("nombrePaleta")
-
-/* for (var i=0; i <pname.length; i++){
-    let match = product [i] getElementsById('nombrePaleta')[0];
-}
- */
-if(match){
-   let textvalue = match.textContent || match.innerHTML
-
-if(textvalue.toUpperCase().indexof(searchbox) > -1){
-    product [i].style.display = "";
-
-}else{
-    product [i].style.display = "none";
-}
-
-}
-
-};
+    const searchbox = document.getElementById("search-item").value.toUpperCase();
+    const storeItems = document.getElementById("contenedorPaletas");
+    const product = document.querySelectorAll(".product");
+    const pname = storeItems.getElementsByTagName("h2");
+  
+    for (var i = 0; i < pname.length; i++) {
+      let match = product[i].getElementsByTagName("h2")[0];
+  
+      if (match) {
+        let textvalue = match.textContent || match.innerHTML;
+  
+        if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+          product[i].style.display = "";
+        } else {
+          product[i].style.display = "none";
+        }
+      }
+    }
+  };
+  
